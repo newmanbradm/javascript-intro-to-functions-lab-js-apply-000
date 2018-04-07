9c9f07792a8351a160f7b48b84936040501db007
Brad Newman <newmanbradm@gmail.com>
2018-04-07 20:34:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,11 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if()
+  if(string.toLowerCase === string) {
+    return "I can't hear you!"
+  }
+  if(string.toUpperCase === string) {
+    return "YES INDEED!"
+  }
+  if 
 }
